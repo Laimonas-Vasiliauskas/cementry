417f390320ad399c36c28bbed29ae1cdc76b2057
Laimonas-Vasiliauskas <232161150+Laimonas-Vasiliauskas@users.noreply.github.com>
2026-07-10 11:51:45 +0300

    contact page changes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../shared/components/hero/hero';
+import { ServiceCard } from '../../shared/components/service-card/service-card';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, ServiceCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
--- src/app/shared/components/service-card/service-card.ts
+++ src/app/shared/components/service-card/service-card.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './service-card.html',
   styleUrl: './service-card.css',
 })
-export class ServiceCard {}
+export class ServiceCard {
+  @Input() title = '';
+  @Input() description = '';
+  @Input() link = '';
+  @Input() image = '';
+}
