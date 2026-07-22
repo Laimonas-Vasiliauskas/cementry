8504a687cc8d932051fc36d536d779e0406dd75d
Laimonas-Vasiliauskas <232161150+Laimonas-Vasiliauskas@users.noreply.github.com>
2026-07-22 12:41:48 +0300

    portfolio style fix

--- src/app/pages/portfolio/portfolio.ts
+++ src/app/pages/portfolio/portfolio.ts
@@ -4,6 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-portfolio',
   imports: [],
   templateUrl: './portfolio.html',
-  styleUrl: './portfolio.css',
+  styleUrl: './portfolio.css'
 })
-export class Portfolio {}
+export class Portfolio {
+  selectedImage: string | null = null;
+
+  openImage(image: string): void {
+    this.selectedImage = image;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeImage(): void {
+    this.selectedImage = null;
+    document.body.style.overflow = '';
+  }
+}
